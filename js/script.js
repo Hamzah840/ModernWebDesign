ff7c40fc874c286fc1b6b355bde23e50a13fcad4
Hamzah Haseeb <hamzahhaseeb840@gmail.com>
2024-05-30 11:40:31 +0530

    changes in script

--- js/script.js
+++ js/script.js
@@ -4,27 +4,36 @@ document.addEventListener("DOMContentLoaded", (event) => {
     smooth: true,
   });
 
-  // This function works for when the mouse is moved
-  //   function mouseFollower() {
-  window.addEventListener("mousemove", (e) => {
+  function mouseFollower(x,y) {
     let mouseFollower = document.querySelector("#mouseFollower");
-    mouseFollower.style.transform = `translate(${e.clientX - 5}px, ${
-      e.clientY - 5
-    }px)`;
-  });
-  //   }
-  //   mouseFollower();
+    mouseFollower.style.transform = `translate(${x - 5}px, ${y - 5}px)`;
+  }
+
   // This function works for when the mouse is moved
+  window.addEventListener("mousemove", (e) => {
+    mouseFollower(e.clientX,e.clientY);
+  });
+  // This function works for when the mouse is moved
+  
+  // This function works for when the TOUCH is moved
+  window.addEventListener("touchmove", (e) => {
+      e.preventDefault();
+      let touch = e.touches[0];
+      mouseFollower(touch.clientX, touch.clientY);
+    },
+    { passive: false }
+  );
+  // This function works for when the TOUCH is moved
 
   tl = gsap.timeline({ defaults: { duration: 0.5, ease: "expo.InOut" } });
 
-  tl.to(".logo", {y: 0});
+  tl.to(".logo", { y: 0 });
 
-  tl.to(".menu-toggler", {y: -10}, "-=100%");
+  tl.to(".menu-toggler", { y: -10 }, "-=100%");
 
-  tl.to(".showTextOnloadFromBottom", {y: 0, stagger: 0.3});
+  tl.to(".showTextOnloadFromBottom", { y: 0, stagger: 0.3 });
 
-  tl.to(".showTextOnloadFromTop", {y: 0, stagger: 0.3});
+  tl.to(".showTextOnloadFromTop", { y: 0, stagger: 0.3 });
 
-  tl.to(".opacityVisibleOnload", {opacity: 1}, "-=100%");
+  tl.to(".opacityVisibleOnload", { opacity: 1 }, "-=100%");
 });
