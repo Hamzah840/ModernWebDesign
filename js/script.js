00f532dfbbc77f2b4915f12c87476edf3bbd7e32
Hamzah Haseeb <hamzahhaseeb840@gmail.com>
2024-05-31 16:39:15 +0530

    updated styles and scripts

--- js/script.js
+++ js/script.js
@@ -4,24 +4,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     smooth: true,
   });
 
-  function mouseFollower(x,y) {
-    let mouseFollower = document.querySelector("#mouseFollower");
-    mouseFollower.style.transform = `translate(${x - 5}px, ${y - 5}px)`;
-  }
 
-  // This function works for when the mouse is moved
-  window.addEventListener("mousemove", (e) => {
-    mouseFollower(e.clientX,e.clientY);
-  });
-  // This function works for when the mouse is moved
-  
-  // This function works for when the TOUCH is moved
-  window.addEventListener("touchmove", (e) => {
-      let touch = e.touches[0];
-      mouseFollower(touch.clientX, touch.clientY);
-    }
-  );
-  // This function works for when the TOUCH is moved
 
   tl = gsap.timeline({ defaults: { duration: 0.5, ease: "expo.InOut" } });
 
@@ -34,4 +17,55 @@ document.addEventListener("DOMContentLoaded", (event) => {
   tl.to(".showTextOnloadFromTop", { y: 0, stagger: 0.3 });
 
   tl.to(".opacityVisibleOnload", { opacity: 1 }, "-=100%");
+
+
+
+  // MOUSE FOLLOWER CIRCLE
+  let circle = document.querySelector("#mouseFollower");
+  let timeout,
+    xprev = 0,
+    yprev = 0;
+
+
+  function mouseFollower(e) {
+    clearTimeout(timeout);
+
+    xscale = Math.abs(e.clientX - xprev);
+    yscale = Math.abs(e.clientY - yprev);
+    let x = gsap.utils.clamp(0.8, 1.2, xscale);
+    let y = gsap.utils.clamp(0.8, 1.2, yscale);
+
+    circle.style.transform = `translate(${e.clientX - 5}px, ${
+      e.clientY - 5
+    }px) scale(${x}, ${y})`;
+
+    xprev = e.clientX;
+    yprev = e.clientY;
+
+    timeout = setTimeout(() => {
+      circle.style.transform = `translate(${e.clientX - 5}px, ${
+        e.clientY - 5
+      }px) scale(1,1)`;
+    }, 100);
+  }
+  window.addEventListener("mousemove", mouseFollower);
+  // MOUSE FOLLOWER CIRCLE
+
+
+
+  // MOUSE LEAVING WINDOW
+  window.addEventListener("mouseout", (e)=>{
+    if(!e.relatedTarget && e.currentTarget === window){
+      clearTimeout(timeout);
+      circle.style.transform = `translate(${e.clientX - 5}px, ${
+        e.clientY - 5
+      }px) scale(0,0)`;
+    }
+    });
+  // MOUSE LEAVING WINDOW
+
+  // IMAGE MOVEMENT
+  // IMAGE MOVEMENT
+
+  
 });
